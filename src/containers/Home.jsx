8e47186cc1812801f093c3f96b5a6ddad1cc2924
sjhogan2 <shannon.jane.hogan@gmail.com>
2017-07-19 22:13:40 -0700

    Create basic home container

---
+++ src/containers/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const HomeContainer = () => {
+  return (
+    <div className="App">
+      <div className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2>Welcome to Break Path</h2>
+      </div>
+    </div>
+  );
+};
+
+export default HomeContainer;
