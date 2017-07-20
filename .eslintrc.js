b37c3849c886e4e221db2db23673ec32a2660c64
sjhogan2 <shannon.jane.hogan@gmail.com>
2017-07-19 21:24:01 -0700

    Add eslint and redux to modules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,33 @@
 module.exports = {
-    "extends": "standard"
+  "extends": [
+    "eslint:recommended",
+    "plugin:react/recommended"
+  ],
+  "env": {
+    "browser": true,
+    "commonjs": true,
+    "es6": true,
+    "jest": true,
+    "mocha": true,
+    "node": true
+  },
+  "parser": "babel-eslint",
+  "parserOptions": {
+    "ecmaVersion": 6,
+    "sourceType": "module",
+    "ecmaFeatures": {
+      "experimentalObjectRestSpread": true,
+      "jsx": true,
+      "modules": true
+    }
+  },
+  "plugins": [
+    "react"
+  ],
+  "rules": {
+    "arrow-body-style": [1],
+    "no-console": [1],
+    "strict": ["error", "global"],
+    "curly": "warn"
+  }
 };
