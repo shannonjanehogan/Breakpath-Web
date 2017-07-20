e73b3560a90135ba24674eaa9d07df95ea2fc1ce
sjhogan2 <shannon.jane.hogan@gmail.com>
2017-07-19 23:03:18 -0700

    Refactor app to use new components

--- src/App.js
+++ src/App.js
@@ -1,53 +1,79 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-// import ThemeDefault from '../theme-default';
+import { Route, Switch } from 'react-router';
+import PropTypes from 'prop-types';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import { LARGE, SMALL } from 'material-ui/utils/withWidth';
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import Header from './components/Header.jsx';
+import LeftDrawer from './components/LeftDrawer.jsx';
+import Home from './containers/Home.jsx';
 
 class App extends Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     navDrawerOpen: false,
-  //   };
-  //   this.handleChangeRequestNavDrawer = this.handleChangeRequestNavDrawer.bind(this);
-  // }
-  //
-  // componentWillReceiveProps(nextProps) {
-  //   if (this.props.width !== nextProps.width) {
-  //     this.setState({ navDrawerOpen: nextProps.width === LARGE });
-  //   }
-  // }
 
-  // handleChangeRequestNavDrawer() {
-  //   this.setState({
-  //     navDrawerOpen: !this.state.navDrawerOpen,
-  //   });
-  // }
-  //
-  // renderLeftDrawer() {
-  //   const navDrawerOpen = this.state.navDrawerOpen;
-  //   const { isLoggedIn } = this.props;
-  //   if (isLoggedIn) {
-  //     return (
-  //       <LeftDrawer
-  //         navDrawerOpen={navDrawerOpen}
-  //         menus={[]}
-  //       />
-  //     );
-  //   }
-  //   return null;
-  // }
+  constructor(props) {
+    super(props);
+    this.state = {
+      drawerOpen: false,
+    };
+    this.handleChangeRequestNavDrawer = this.handleChangeRequestNavDrawer.bind(this);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (this.props.width !== nextProps.width) {
+      this.setState({ drawerOpen: nextProps.width === LARGE });
+    }
+  }
+
+  handleChangeRequestNavDrawer() {
+    this.setState({
+      drawerOpen: !this.state.drawerOpen,
+    });
+  }
+
+  renderLeftDrawer() {
+    return (
+      <LeftDrawer
+        drawerOpen={this.state.drawerOpen}
+      />
+    );
+  }
 
   render() {
+    const drawerOpen = this.state.drawerOpen;
+    const paddingLeftDrawerOpen = 236;
+    const styles = {
+      header: {
+        paddingLeft: drawerOpen ? paddingLeftDrawerOpen : 0,
+      },
+      container: {
+        margin: '80px 20px 20px 15px',
+        paddingLeft: drawerOpen && this.props.width !== SMALL ? paddingLeftDrawerOpen : 0,
+      },
+    };
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to Break Path</h2>
+      <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
+        <div>
+          <Header
+            styles={styles.header}
+            handleChangeRequestNavDrawer={this.handleChangeRequestNavDrawer}
+          />
+          {this.renderLeftDrawer()}
+          <div style={styles.container}>
+            <Switch>
+              <Route exact path="/" component={Home} />
+            </Switch>
+          </div>
         </div>
-      </div>
+      </MuiThemeProvider>
     );
   }
 }
+
+App.propTypes = {
+  children: PropTypes.element,
+  width: PropTypes.number,
+  isLoggedIn: PropTypes.bool,
+};
 
 export default App;
