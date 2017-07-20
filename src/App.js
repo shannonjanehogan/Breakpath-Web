e797fab27afad8d0f4e8aab22043de10b6157bae
sjhogan2 <shannon.jane.hogan@gmail.com>
2017-07-19 20:45:49 -0700

    Remove apollo from application
    - Switching to redux to start

--- src/App.js
+++ src/App.js
@@ -1,48 +1,51 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {
-  ApolloClient,
-  gql,
-  graphql,
-  ApolloProvider,
-} from 'react-apollo';
-const client = new ApolloClient();
-
-const ChannelsList = ({ data: {loading, error, channels }}) => {
-   if (loading) {
-     return <p>Loading ...</p>;
-   }
-   if (error) {
-     return <p>{error.message}</p>;
-   }
-   return <ul>
-     { channels.map( ch => <li key={ch.id}>{ch.name}</li> ) }
-   </ul>;
- };
-
-const channelsListQuery = gql`
-   query ChannelsListQuery {
-     channels {
-       id
-       name
-     }
-   }
- `;
-const ChannelsListWithData = graphql(channelsListQuery)(ChannelsList);
+import ThemeDefault from '../theme-default';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      navDrawerOpen: false,
+    };
+    this.handleChangeRequestNavDrawer = this.handleChangeRequestNavDrawer.bind(this);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (this.props.width !== nextProps.width) {
+      this.setState({ navDrawerOpen: nextProps.width === LARGE });
+    }
+  }
+
+  handleChangeRequestNavDrawer() {
+    this.setState({
+      navDrawerOpen: !this.state.navDrawerOpen,
+    });
+  }
+
+  renderLeftDrawer() {
+    const navDrawerOpen = this.state.navDrawerOpen;
+    const { isLoggedIn } = this.props;
+    if (isLoggedIn) {
+      return (
+        <LeftDrawer
+          navDrawerOpen={navDrawerOpen}
+          menus={[]}
+        />
+      );
+    }
+    return null;
+  }
+
   render() {
     return (
-      <ApolloProvider client={client}>
-        <div className="App">
-          <div className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h2>Welcome to Break Path</h2>
-          </div>
-           <ChannelsListWithData />
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to Break Path</h2>
         </div>
-      </ApolloProvider>
+      </div>
     );
   }
 }
