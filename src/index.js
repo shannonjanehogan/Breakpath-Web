883e3ba0bd99542a2c4ce019002f269c280e4248
sjhogan2 <shannon.jane.hogan@gmail.com>
2017-07-19 23:04:27 -0700

    Add react router

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom'
+import injectTapEventPlugin from 'react-tap-event-plugin';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// https://github.com/callemall/material-ui/issues/4670
+// Needed for onTouchTap
+injectTapEventPlugin();
+
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root')
+);
 registerServiceWorker();
