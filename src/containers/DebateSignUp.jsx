078058efd1159d8e1704fc634a137e0571fb1ab6
sjhogan2 <shannon.jane.hogan@gmail.com>
2017-07-25 10:45:33 -0700

    Add select fields for debate preference and skill level

--- src/containers/DebateSignUp.jsx
+++ src/containers/DebateSignUp.jsx
@@ -1,65 +1,105 @@
 import React, { Component } from 'react';
 import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
+import SelectField from 'material-ui/SelectField';
+import MenuItem from 'material-ui/MenuItem';
 
 class DebateSignUp extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      name: '',
-      partnerPreference: null,
-      debatePreference: 'DebateOrJudge',
-      pro: false,
+      preferences: {
+        first_name: '',
+        last_name: '',
+        partner_preference: null,
+        debate_preference: 'debate_or_judge',
+        skill_level: 'nov',
+      },
     };
-    this.onSaveClick = this.onSaveClick.bind(this);
-    this.onFormTextUpdate = this.onFormTextUpdate.bind(this);
+    this.handleDebatePreferenceChange = this.handleDebatePreferenceChange.bind(this);
+    this.handleSkillLevelChange = this.handleSkillLevelChange.bind(this);
+    this.handleDebateSignUpClick = this.handleDebateSignUpClick.bind(this);
   }
 
-  onSaveClick(event) {
+  handleDebatePreferenceChange = (event, index, value) => {
+    const state = this.state;
+    state.preferences.debate_preference = value;
+    this.setState({ state });
+  }
+
+  handleSkillLevelChange = (event, index, value) => {
+    const state = this.state;
+    state.preferences.skill_level = value;
+    this.setState({ state });
+  }
+
+  handleDebateSignUpClick(event) {
     event.preventDefault();
-    const field = event.target.name;
-    const state = this.stat;
-    state[field] = event.target.value;
-    return this.setState({ state });
-
-  }
-
-  onFormTextUpdate(event) {
-
+    // TODO call redux login action
   }
 
   render() {
-    const debatePreferences = ['debate', 'judge', 'either'];
-    return(
-      <form>
-        <TextField>
-          key={0}
-          hintText={'Name'}
-          errorText={''}
-          errorStyle={{ float: 'left' }}
-          name={'Name'}
-          floatingLabelText={'Name'}
-          onChange={this.onFormTextUpdate}
-          value={this.state.name}
-        </TextField>
-        <TextField>
-          key={0}
-          hintText={'Pro or nov?'}
-          errorText={''}
-          errorStyle={{ float: 'left' }}
-          name={'Name'}
-          floatingLabelText={'Name'}
-          onChange={this.onFormTextUpdate}
-          value={this.state.pro}
-        </TextField>
-        <RaisedButton
-          label="Save"
-          style={{ width: '10px', marginTop: '30px' }}
-          onClick={this.onSaveClick}
-          disabled={false}
-        />
-      </form>
+    const debatePreferenceSelectField =  (
+      <SelectField
+        style={{margin: 'auto', width: '100%'}}
+        floatingLabelText="Debate Preference"
+        value={this.state.preferences.debate_preference}
+        onChange={this.handleDebatePreferenceChange}
+      >
+        <MenuItem value={'debate'} primaryText="Debate" />
+        <MenuItem value={'judge'} primaryText="Judge" />
+        <MenuItem value={'debate_or_judge'} primaryText="Indifferent" />
+      </SelectField>
+    );
+    const skillLevelSelectField =  (
+      <SelectField
+        style={{margin: 'auto', width: '100%'}}
+        floatingLabelText="Skill Level"
+        value={this.state.preferences.skill_level}
+        onChange={this.handleSkillLevelChange}
+      >
+        <MenuItem value={'nov'} primaryText="Novice" />
+        <MenuItem value={'pro'} primaryText="Pro" />
+        <MenuItem value={'advanced'} primaryText="Worlds" />
+      </SelectField>
+    );
+    return (
+      <div style={{margin: 'auto', width: '100%'}}>
+        <div style={{margin: 'auto', width: '50%'}}>
+           <TextField
+             style={{margin: 'auto', width: '100%'}}
+             hintText="Enter your first name"
+             floatingLabelText="First Name"
+             onChange = {(event, newValue) => this.setState({ first_name: newValue })}
+           />
+           <br/>
+           <TextField
+             style={{margin: 'auto', width: '100%'}}
+             hintText="Enter your last name"
+             floatingLabelText="Last Name"
+             onChange = {(event, newValue) => this.setState({ last_name: newValue })}
+           />
+           <br/>
+           <TextField
+             style={{margin: 'auto', width: '100%'}}
+             hintText="Enter your partner preference"
+             floatingLabelText="Partner Preference"
+             onChange = {(event, newValue) => this.setState({ partner_preference: newValue })}
+           />
+           <br/>
+           {debatePreferenceSelectField}
+           <br/>
+           {skillLevelSelectField}
+           <br/>
+           <RaisedButton
+             label="Sign Up to Debate"
+             primary={true}
+             style={{marginTop: '15px'}}
+             onClick={(event) => this.handleDebateSignUpClick(event)}
+           />
+         </div>
+      </div>
     );
   }
 }
