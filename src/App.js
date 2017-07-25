41cedd338292eab99d4038efe503578f1b446a88
sjhogan2 <shannon.jane.hogan@gmail.com>
2017-07-24 20:48:50 -0700

    Resolve merge conflicts from stash

--- src/App.js
+++ src/App.js
@@ -3,11 +3,10 @@ import { Route, Switch } from 'react-router';
 import PropTypes from 'prop-types';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import { LARGE, SMALL } from 'material-ui/utils/withWidth';
-import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
-import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import Header from './components/Header.jsx';
 import LeftDrawer from './components/LeftDrawer.jsx';
 import Home from './containers/Home.jsx';
+import Login from './containers/Login.jsx';
 
 class App extends Component {
 
@@ -52,7 +51,7 @@ class App extends Component {
       },
     };
     return (
-      <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
+      <MuiThemeProvider>
         <div>
           <Header
             styles={styles.header}
@@ -62,6 +61,10 @@ class App extends Component {
           <div style={styles.container}>
             <Switch>
               <Route exact path="/" component={Home} />
+              <Route
+                path="/login"
+                component={Login}
+              />
             </Switch>
           </div>
         </div>
