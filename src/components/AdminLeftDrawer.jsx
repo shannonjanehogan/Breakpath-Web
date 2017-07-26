1355f07ea4cba600b8b0106edb48f1cadaa12b0e
sjhogan2 <shannon.jane.hogan@gmail.com>
2017-07-25 19:04:27 -0700

    Create admin specific left drawer

---
+++ src/components/AdminLeftDrawer.jsx
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
+import { Link } from 'react-router-dom';
+import SocialIcon from 'material-ui/svg-icons/social/group';
+import { spacing, typography } from 'material-ui/styles';
+
+class AdminLeftDrawer extends Component {
+  render() {
+    const { drawerOpen } = this.props;
+    const styles = {
+      logo: {
+        cursor: 'pointer',
+        fontSize: 22,
+        color: typography.textFullWhite,
+        lineHeight: `${spacing.desktopKeylineIncrement}px`,
+        fontWeight: typography.fontWeightLight,
+        backgroundColor: '#002154',
+        paddingLeft: 40,
+        height: 56,
+      },
+      welcome: {
+        div: {
+          padding: '15px 15px 20px 0',
+          height: 45,
+        },
+        span: {
+          paddingTop: 12,
+          paddingLeft: 40,
+          display: 'block',
+          fontWeight: 300,
+        },
+      },
+    };
+
+    return (
+      <Drawer
+        docked
+        open={drawerOpen}
+        style={{ width: 230 }}
+      >
+        <div style={styles.logo}>
+          UBCDS Admin
+        </div>
+        <div style={styles.welcome.div}>
+          <span style={styles.welcome.span}>
+            Welcome back!
+          </span>
+        </div>
+        <div>
+          <MenuItem
+            key={0}
+            style={{fontSize: 14}}
+            primaryText={'Start Room Sorter'}
+            leftIcon={(<SocialIcon/>)}
+            containerElement={<Link to={'/sorted_rooms/new'} />}
+          />
+        </div>
+      </Drawer>
+    );
+  }
+}
+
+AdminLeftDrawer.propTypes = {
+  drawerOpen: PropTypes.bool,
+};
+
+export default AdminLeftDrawer;
