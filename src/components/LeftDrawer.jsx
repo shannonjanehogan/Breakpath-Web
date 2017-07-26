31bc03e5556dce723b8e106c664c61abe3e3022b
sjhogan2 <shannon.jane.hogan@gmail.com>
2017-07-25 21:09:21 -0700

    Add temporary menu item links to admin pages

--- src/components/LeftDrawer.jsx
+++ src/components/LeftDrawer.jsx
@@ -4,6 +4,8 @@ import Drawer from 'material-ui/Drawer';
 import MenuItem from 'material-ui/MenuItem';
 import { Link } from 'react-router-dom';
 import SocialIcon from 'material-ui/svg-icons/social/group';
+import LocationIcon from 'material-ui/svg-icons/social/location-city';
+import PersonAddIcon from 'material-ui/svg-icons/social/person-add';
 import { spacing, typography } from 'material-ui/styles';
 
 class LeftDrawer extends Component {
@@ -55,6 +57,20 @@ class LeftDrawer extends Component {
             primaryText={'Sign up to Debate'}
             leftIcon={(<SocialIcon/>)}
             containerElement={<Link to={'/debates/new'} />}
+          />
+          <MenuItem
+            key={1}
+            style={{fontSize: 14}}
+            primaryText={'Sorted Rooms'}
+            leftIcon={(<LocationIcon/>)}
+            containerElement={<Link to={'/sorted_rooms'} />}
+          />
+          <MenuItem
+            key={2}
+            style={{fontSize: 14}}
+            primaryText={'New Sorted Room'}
+            leftIcon={(<PersonAddIcon/>)}
+            containerElement={<Link to={'/sorted_rooms/new'} />}
           />
         </div>
       </Drawer>
