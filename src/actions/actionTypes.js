762598d7b91393ebbf07fb7d8eda04fd74611de1
sjhogan2 <shannon.jane.hogan@gmail.com>
2017-08-12 12:34:55 -0700

    Create session actions

---
+++ src/actions/actionTypes.js
@@ -0,0 +1,14 @@
+// Session actions
+export const LOG_IN_SUCCESS = 'LOG_IN_SUCCESS';
+export const LOG_IN_REQUEST = 'LOG_IN_REQUEST';
+export const LOG_IN_ERROR = 'LOG_IN_ERROR';
+export const LOG_OUT_SUCCESS = 'LOG_OUT_SUCCESS';
+
+export const SIGN_UP_SUCCESS = 'SIGN_UP_SUCCESS';
+export const SIGN_UP_REQUEST = 'SIGN_UP_REQUEST';
+export const SIGN_UP_ERROR = 'SIGN_UP_ERROR';
+
+// Signup to debate actions
+
+
+// Sorted Rooms actions
