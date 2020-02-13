5bfed0229ff8cd04d147a455bc10ca4294fc8410
shannonjanehogan <shannon.jane.hogan@gmail.com>
2020-02-13 15:23:46 -0800

    Fix broken test- add mock localStorage

--- src/App.test.js
+++ src/App.test.js
@@ -7,6 +7,13 @@ import App from './App';
 it('renders without crashing', () => {
   injectTapEventPlugin();
   const div = document.createElement('div');
+  const localStorageMock = {
+    getItem: jest.fn(),
+    setItem: jest.fn(),
+    removeItem: jest.fn(),
+    clear: jest.fn(),
+  };
+  global.localStorage = localStorageMock;
 
   ReactDOM.render(
     <BrowserRouter>
